6660840cccda71110c9aee27a5a3077bca5a189d
PranjalAgrawal <pranjal605@gmail.com>
2024-05-19 19:42:40 +0530

    Code Refactoring

--- src/Conponents/NavMenu.js
+++ src/Conponents/NavMenu.js
@@ -8,7 +8,7 @@ export default function NavMenu() {
     return (
       <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3 mx-3" container light>
         <Navbar.Brand> 
-          <a href="/" className="nav-link">IrctcScrpe</a>
+          <a href="/" className="nav-link">Deep Scan Train</a>
         </Navbar.Brand>
         
         <Navbar.Collapse className="d-sm-inline-flex flex-sm-row-reverse">
